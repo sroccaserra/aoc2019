3640881f09563a6e5e9a8faa404b4038a3942777
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-15 12:39:25 +0100

    Add main entry point for day 1

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}
+             :day-01 {:main advent-of-code-2019.day-01.main}}
+  :aliases {"run-day-01" ["with-profile" "day-01" "run"]})
--- src/advent_of_code_2019/day_01/main.clj
+++ src/advent_of_code_2019/day_01/main.clj
@@ -1,4 +1,8 @@
-(ns advent-of-code-2019.day-01.main)
+(ns advent-of-code-2019.day-01.main
+  (:gen-class))
+
+(defn -main [& args]
+  (println args))
 
 (defn fuel-for-module [mass]
   (- (quot mass 3) 2))
