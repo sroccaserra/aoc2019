5f722ea8450c508efd7ddc94edff09da922b6e3f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-15 15:07:05 +0100

    Read lines from stdin

--- src/advent_of_code_2019/day_01/main.clj
+++ src/advent_of_code_2019/day_01/main.clj
@@ -1,8 +1,12 @@
 (ns advent-of-code-2019.day-01.main
   (:gen-class))
 
+(defn read-lines-from-stdin []
+  (line-seq (java.io.BufferedReader. *in*)))
+
 (defn -main [& args]
-  (println args))
+  (doseq [line (read-lines-from-stdin)]
+    (println line)))
 
 (defn fuel-for-module [mass]
   (- (quot mass 3) 2))
