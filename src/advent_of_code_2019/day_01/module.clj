0fc4c89fe09084788c8404cb329f55448e9abc61
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-15 17:30:06 +0100

    Complete day 1 part 2

--- src/advent_of_code_2019/day_01/module.clj
+++ src/advent_of_code_2019/day_01/module.clj
@@ -6,9 +6,15 @@
       (- 2)))
 
 (defn fuel-for-module-mass [module-mass]
-  (fuel-for-mass module-mass))
+  (loop [sum 0
+         remaining-mass module-mass]
+    (let [new-required-fuel (fuel-for-mass remaining-mass)]
+      (if (<= new-required-fuel 0)
+        sum
+        (recur (+ sum new-required-fuel)
+               new-required-fuel)))))
 
 (defn compute-required-fuel-for-module-masses [module-masses]
   (->> module-masses
-      (map fuel-for-module-mass)
-      (apply +)))
+       (map fuel-for-module-mass)
+       (apply +)))
--- test/advent_of_code_2019/day_01/module_test.clj
+++ test/advent_of_code_2019/day_01/module_test.clj
@@ -16,21 +16,18 @@
     (is (= 33583 (fuel-for-mass 100756)))))
 
 (deftest computing-fuel-for-one-module
-  (testing "fuel for module of mass 12"
-    (is (= 2 (fuel-for-module-mass 12))))
-
   (testing "fuel for module of mass 14"
     (is (= 2 (fuel-for-module-mass 14))))
 
   (testing "fuel for module of mass 1969"
-    (is (= 654 (fuel-for-module-mass 1969))))
+    (is (= 966 (fuel-for-module-mass 1969))))
 
   (testing "fuel for module of mass 100756"
-    (is (= 33583 (fuel-for-module-mass 100756)))))
+    (is (= 50346 (fuel-for-module-mass 100756)))))
 
 (deftest computing-total-required-fuel
   (testing "required fuel for two modules"
     (is (= 4 (compute-required-fuel-for-module-masses [12 14]))))
 
   (testing "required fuel for three modules"
-    (is (= 658 (compute-required-fuel-for-module-masses [12 14 1969])))))
+    (is (= 970 (compute-required-fuel-for-module-masses [12 14 1969])))))
