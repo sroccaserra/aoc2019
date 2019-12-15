4ae5ed4125ba273261f33e5d5e0f14c258e69939
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-15 19:02:17 +0100

    Parse the opcodes from stdin

--- src/advent_of_code_2019/day_02/main.clj
+++ src/advent_of_code_2019/day_02/main.clj
@@ -1,9 +1,14 @@
 (ns advent-of-code-2019.day-02.main
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn read-ints-from-line [line]
+   (map #(Integer/parseInt %) (str/split line #",")))
 
 (defn read-lines-from-stdin []
   (line-seq (java.io.BufferedReader. *in*)))
 
 (defn -main [& args]
-  (println (read-lines-from-stdin)))
-
+  (println (-> (read-lines-from-stdin)
+               first
+               read-ints-from-line)))
