548d41a7ea8c9f4f8c2eff8550fdf2302ffbf12c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-21 00:30:38 +0100

    Complete day 4 part 2
    I was super lazy on this one :D

--- src/day_04/password.clj
+++ src/day_04/password.clj
@@ -7,6 +7,25 @@
       (= (nth digits 3) (nth digits 4))
       (= (nth digits 4) (nth digits 5))))
 
+(defn has-exactly-two-identical-adjacent-numbers [digits]
+  (or (and (= (nth digits 0) (nth digits 1))
+           (not= (nth digits 1) (nth digits 2)))
+
+      (and (not= (nth digits 0) (nth digits 1))
+           (= (nth digits 1) (nth digits 2))
+           (not= (nth digits 2) (nth digits 3)))
+
+      (and (not= (nth digits 1) (nth digits 2))
+           (= (nth digits 2) (nth digits 3))
+           (not= (nth digits 3) (nth digits 4)))
+
+      (and (not= (nth digits 2) (nth digits 3))
+           (= (nth digits 3) (nth digits 4))
+           (not= (nth digits 4) (nth digits 5)))
+
+      (and (not= (nth digits 3) (nth digits 4))
+           (= (nth digits 4) (nth digits 5)))))
+
 (defn digits-never-decrease [digits]
   (and (<= (nth digits 0) (nth digits 1))
        (<= (nth digits 1) (nth digits 2))
@@ -19,7 +38,12 @@
        str
        (map (comp read-string str))))
 
-(defn count-possible-passwords-in-range []
+(defn count-possible-passwords-in-range-part-1 []
   (count (filter #(and (has-two-identical-adjacent-numbers %)
+                       (digits-never-decrease %))
+                 (map number->digits (range 138241 (inc 674034))))))
+
+(defn count-possible-passwords-in-range-part-2 []
+  (count (filter #(and (has-exactly-two-identical-adjacent-numbers %)
                        (digits-never-decrease %))
                  (map number->digits (range 138241 (inc 674034))))))
