1ece70855815c9fc30b5e3fb7bb0e3b74469e923
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-23 11:27:30 +0100

    Introduce add and mul instructions

--- src/day_05/intcode.clj
+++ src/day_05/intcode.clj
@@ -27,14 +27,26 @@
   (= (:opcode halt-instruction)
      (read-opcode vm-state)))
 
+(defn create-add-instruction [vm-state]
+  {:opcode 1
+   :parameter-1 (parameter-value vm-state 1)
+   :parameter-2 (parameter-value vm-state 2)
+   :dest (parameter-address vm-state 3)
+   :size 4})
+
+(defn create-mul-instruction [vm-state]
+  {:opcode 2
+   :parameter-1 (parameter-value vm-state 1)
+   :parameter-2 (parameter-value vm-state 2)
+   :dest (parameter-address vm-state 3)
+   :size 4})
+
 (defn read-instruction [vm-state]
-  (if (halted? vm-state)
-    halt-instruction
-    {:opcode (read-opcode vm-state)
-     :parameter-1 (parameter-value vm-state 1)
-     :parameter-2 (parameter-value vm-state 2)
-     :dest (parameter-address vm-state 3)
-     :size 4}))
+  (let [opcode (read-opcode vm-state)]
+    (condp = opcode
+      (:opcode halt-instruction) halt-instruction
+      1 (create-add-instruction vm-state)
+      2 (create-mul-instruction vm-state))))
 
 (defn execute-instruction [{:keys [opcode size parameter-1 parameter-2 dest]} vm-state]
   (let [operation (get operations opcode)
