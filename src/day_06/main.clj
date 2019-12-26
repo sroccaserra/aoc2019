94ebb3a36830856772608ec4ab762afae1933ba2
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-26 15:36:40 +0100

    Complete day 6 part 1

--- src/day_06/main.clj
+++ src/day_06/main.clj
@@ -4,8 +4,18 @@
             [clojure.edn :as edn]
             [aoc-common-cli :refer [read-lines-from-stdin]]))
 
+(defn count-ancestors
+  ([parent-map orbiter]
+   (count-ancestors parent-map orbiter 0))
+  ([parent-map orbiter n]
+   (let [ancestor (parent-map orbiter)]
+     (if (nil? ancestor)
+       n
+       (recur parent-map ancestor (inc n))))))
+
 (defn -main [& args]
-  (-> (read-lines-from-stdin)
+  (let [parent-map (-> (read-lines-from-stdin)
       str/join
-      edn/read-string
-      println))
+      edn/read-string)
+        orbiters (keys parent-map)]
+    (println (reduce + (map #(count-ancestors parent-map %) orbiters)))))
