d936e3a26fb8af8a807a7c14719e3f44ffe58665
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-29 22:36:07 +0100

    Complete day 9 part 2

--- src/day_09/main.clj
+++ src/day_09/main.clj
@@ -12,8 +12,12 @@
 (defn -main [& args]
   (let [program (-> (read-lines-from-stdin)
                     first
-                    read-ints-from-line)
-        vm (create-intcode-vm program :inputs [1] :memory-size 1028)]
-    (println (-> vm
-                 run
-                 :outputs))))
+                    read-ints-from-line)]
+    (-> (create-intcode-vm program :inputs [1] :memory-size 1028)
+        run
+        :outputs
+        println)
+    (-> (create-intcode-vm program :inputs [2] :memory-size 1076)
+        run
+        :outputs
+        println)))
