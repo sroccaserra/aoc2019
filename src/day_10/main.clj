19f406539a5839af4fc5b70fe1ce91da4934b928
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-30 12:43:04 +0100

    Try simple count function (missing some asteroids)

--- src/day_10/main.clj
+++ src/day_10/main.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as str]
             [aoc-common-cli :refer [read-lines-from-stdin]]))
 
-(defn asteroid-coordinates [lines]
+(defn parse-asteroid-positions [lines]
   (apply concat
          (for [[row-number row] (map-indexed vector lines)]
            (keep-indexed #(when (= \# %2) {:x %1 :y row-number}) row))))
@@ -11,11 +11,27 @@
 (defn slope [point-1 point-2]
   (let [dx (apply - (map :x [point-2 point-1]))
         dy (apply - (map :y [point-2 point-1]))]
-    (if (zero? dx)
-      (if (pos? dy)
-        :infinite
-        :-infinite)
-      (/ dy dx))))
+    (cond
+      (and (zero? dx) (zero? dy)) nil
+      (zero? dx) (if (pos? dy)
+                   :infinite
+                   :-infinite)
+      (zero? dy) (if (pos? dx)
+                   :zero
+                   :-zero)
+      :else (/ dy dx))))
+
+(defn count-unique-slopes [asteroid asteroids]
+  (->> (map #(slope asteroid %) asteroids)
+       (filter (comp not nil?))
+       set
+       count))
+
+(defn count-detected-from-best-location [asteroids]
+  (->> asteroids
+       (map #(count-unique-slopes % asteroids))
+       (apply max)))
 
 (defn -main [& args]
-  (println (asteroid-coordinates (read-lines-from-stdin))))
+  (let [asteroids (parse-asteroid-positions (read-lines-from-stdin))]
+    (println (count-detected-from-best-location asteroids))))
