14d1c926ff1901620c0483404a763aa832f1b57f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-30 10:02:18 +0100

    List asteroid coordinates

--- src/day_10/main.clj
+++ src/day_10/main.clj
@@ -3,6 +3,10 @@
   (:require [clojure.string :as str]
             [aoc-common-cli :refer [read-lines-from-stdin]]))
 
+(defn asteroid-coordinates [lines]
+  (apply concat
+         (for [[row-number row] (map-indexed vector lines)]
+           (keep-indexed #(when (= \# %2) {:x %1 :y row-number}) row))))
+
 (defn -main [& args]
-  (doseq [line (read-lines-from-stdin)]
-    (println line)))
+  (println (asteroid-coordinates (read-lines-from-stdin))))
