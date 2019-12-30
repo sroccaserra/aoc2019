3f1ef6c34fa6888f6a866392b9a99f7b84c81da4
Sébastien Roccaserra <sroccaserra@yahoo.com>
2019-12-30 10:25:45 +0100

    Compute the slope between two points

--- src/day_10/main.clj
+++ src/day_10/main.clj
@@ -8,5 +8,14 @@
          (for [[row-number row] (map-indexed vector lines)]
            (keep-indexed #(when (= \# %2) {:x %1 :y row-number}) row))))
 
+(defn slope [point-1 point-2]
+  (let [dx (apply - (map :x [point-2 point-1]))
+        dy (apply - (map :y [point-2 point-1]))]
+    (if (zero? dx)
+      (if (pos? dy)
+        :infinite
+        :-infinite)
+      (/ dy dx))))
+
 (defn -main [& args]
   (println (asteroid-coordinates (read-lines-from-stdin))))
