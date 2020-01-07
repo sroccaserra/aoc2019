90dcbb6336c7a49004c04c15ab01b117f01c10e8
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-01-07 22:43:40 +0100

    Display screen

--- src/day_13/main.clj
+++ src/day_13/main.clj
@@ -1,15 +1,48 @@
 (ns day-13.main
   (:gen-class)
-  (:require [aoc-common-cli :refer [read-intcode-program-from-stdin]]
-            [intcode.vm-state :refer [create-intcode-vm]]
-            [intcode.run :refer [run]]))
+  (:require [clojure.string :as str]
+            [aoc-common-cli :refer [read-intcode-program-from-stdin]]
+            [intcode.vm-state :refer [create-intcode-vm add-input]]
+            [intcode.run :refer [run run-until-needs-input]]))
+
+(def tiles {0 \.
+            1 \█
+            2 \▒
+            3 \=
+            4 \o})
+
+(def w 42)
+(def h 24)
+
+(def empty-screen (into [] (repeat (* w h) \?)))
+
+(defn screen-to-str [screen]
+  (->> screen
+       (partition w)
+       (map #(apply str %))
+       (str/join "\n")))
+
+(defn draw-tile-to-screen [screen [x y tile-id]]
+  (assoc screen (+ (* y w) x) (tiles tile-id)))
+
+(defn draw-tiles-to-screen [screen tiles]
+  (if (empty? tiles)
+    screen
+    (recur (draw-tile-to-screen screen (first tiles))
+           (rest tiles))))
 
 (defn -main [& args]
   (let [program (read-intcode-program-from-stdin)
         vm (create-intcode-vm program :memory-size 2659)]
     (prn (->> (run vm)
-             :outputs
-             rest rest
-             (take-nth 3)
-             (filter #(= 2 %))
-             count))))
+              :outputs
+              (drop 2)
+              (take-nth 3)
+              (filter #(= 2 %))
+              count))
+    (println (->> (add-input vm 2)
+                  run-until-needs-input
+                  :outputs
+                  (partition 3)
+                  (draw-tiles-to-screen empty-screen)
+                  screen-to-str))))
