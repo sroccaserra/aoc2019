83859d3c8c2ff79589fc572a762236e23e0d4995
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-01-08 13:53:06 +0100

    Complete day 13 part 2

--- src/day_13/main.clj
+++ src/day_13/main.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as str]
             [aoc-common-cli :refer [read-intcode-program-from-stdin]]
-            [intcode.vm-state :refer [create-intcode-vm add-input]]
+            [intcode.vm-state :refer [create-intcode-vm add-input drop-outputs]]
             [intcode.run :refer [run run-until-needs-input]]))
 
 (def tiles {0 \.
@@ -14,22 +14,90 @@
 (def w 42)
 (def h 24)
 
-(def empty-screen (into [] (repeat (* w h) \?)))
+(def empty-vram (into [] (repeat (* w h) \?)))
 
-(defn screen-to-str [screen]
-  (->> screen
+(defn reset-terminal []
+  (println  "\033c"))
+
+(defn vram-to-str [vram]
+  (->> vram
        (partition w)
        (map #(apply str %))
        (str/join "\n")))
 
-(defn draw-tile-to-screen [screen [x y tile-id]]
-  (assoc screen (+ (* y w) x) (tiles tile-id)))
+(defn draw-tile-to-vram [vram [x y tile-id]]
+  (assoc vram (+ (* y w) x) (tiles tile-id)))
 
-(defn draw-tiles-to-screen [screen tiles]
-  (if (empty? tiles)
-    screen
-    (recur (draw-tile-to-screen screen (first tiles))
-           (rest tiles))))
+(defn has-block-tiles [vram]
+  (some #(= (tiles 2) %) vram))
+
+(defn is-score-tile [tile]
+  (= [-1 0] (take 2 tile)))
+
+(defn find-paddle-tile [tiles]
+  (some #(when (= 3 (nth % 2)) %) tiles))
+
+(defn find-ball-tile [tiles]
+  (some #(when (= 4 (nth % 2)) %) tiles))
+
+(defn find-score-tile [tiles]
+  (some #(when (is-score-tile %) %) tiles))
+
+(defn read-output [outputs]
+  (let [tiles (partition 3 outputs)]
+    {:paddle-x (first (find-paddle-tile tiles))
+     :ball-x (first (find-ball-tile tiles))
+     :score (nth (find-score-tile tiles) 2)
+     :tiles (remove is-score-tile tiles)}))
+
+(defn update-vm-state [vm-state {:keys [paddle-x ball-x]}]
+  (let [joystick-state (cond (nil? paddle-x) 0
+                             (< paddle-x ball-x) 1
+                             (> paddle-x ball-x) -1
+                             :else 0)]
+    (prn joystick-state)
+    (-> vm-state
+        drop-outputs
+        (add-input joystick-state))))
+
+(defn draw-game-state [{:keys [:score :vram]}]
+  (reset-terminal)
+  (println (vram-to-str vram))
+  (println "score:" score))
+
+(defn create-game-state []
+  {:paddle-x nil
+   :ball-x nil
+   :score nil
+   :vram empty-vram})
+
+(defn update-vram [{vram :vram :as game-state} tiles]
+  (assoc game-state
+         :vram
+         (reduce draw-tile-to-vram vram tiles)))
+
+(defn not-nil-keys [m ks]
+  (->> (select-keys m ks)
+       (filter #(not (nil? (second %))))))
+
+(defn update-game-state [game-state outputs]
+  (let [new-values (read-output outputs)]
+    (-> game-state
+        (merge (not-nil-keys new-values [:paddle-x :ball-x :score]))
+        (update-vram (:tiles new-values)))))
+
+(defn play-game
+  ([vm-state]
+   (play-game vm-state (create-game-state)))
+  ([vm-state game-state]
+   (let [next-vm-state (run-until-needs-input vm-state)
+         game-state' (update-game-state game-state
+                                        (:outputs next-vm-state))]
+     (draw-game-state game-state')
+     (comment Thread/sleep 25)
+     (if (not (has-block-tiles (:vram game-state')))
+       next-vm-state
+       (recur (update-vm-state next-vm-state game-state') game-state')))))
 
 (defn -main [& args]
   (let [program (read-intcode-program-from-stdin)
@@ -40,9 +108,4 @@
               (take-nth 3)
               (filter #(= 2 %))
               count))
-    (println (->> (add-input vm 2)
-                  run-until-needs-input
-                  :outputs
-                  (partition 3)
-                  (draw-tiles-to-screen empty-screen)
-                  screen-to-str))))
+    (play-game (assoc-in vm [:memory 0] 2))))
