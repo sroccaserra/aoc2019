ce28ee2f9e0a675a078e5a879a91357c9d4b7c26
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-26 20:47:51 +0100

    [day 14] Find a reaction by ingredient name

--- src/day_14/Day14.hs
+++ src/day_14/Day14.hs
@@ -2,13 +2,27 @@ module Day14 where
 
 import Data.Char
 import qualified Data.Map as Map
+import Data.Map ((!))
 import Text.ParserCombinators.ReadP
 
-main = interact $ show.partOne.map parseLine.lines
-
-partOne = id
+partOne reactions = ingredients
+  where ingredients = snd recipe
+        recipe = grimoire ! "FUEL"
+        grimoire = foldl storeByProductName Map.empty reactions
 
 type Reagent = (String, Int)
+
+storeByProductName m r = Map.insert name r m
+  where name = fst $ fst r
+
+---
+-- Main
+
+main = interact $ show . partOne . map parseLine . lines
+
+---
+-- Parsing
+
 parseLine = fst . last . readP_to_S parser
 
 parser = do
