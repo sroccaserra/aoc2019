51f06540466b524327f35829a1bebc0fd87d46f1
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-26 23:45:22 +0100

    [day 14] Ore computation, broken stock management

--- src/day_14/Day14.hs
+++ src/day_14/Day14.hs
@@ -2,15 +2,27 @@ module Day14 where
 
 import Data.Char
 import qualified Data.Map as Map
-import Data.Map ((!))
+import Data.Map (Map, (!), empty)
 import Text.ParserCombinators.ReadP
+import Debug.Trace
 
-partOne reactions = ingredients
-  where ingredients = snd recipe
-        recipe = grimoire ! "FUEL"
-        grimoire = foldl storeByProductName Map.empty reactions
+partOne reactions = oreFor grimoire empty ("FUEL", 1)
+  where grimoire = foldl storeByProductName empty reactions
 
 type Reagent = (String, Int)
+type Reaction = (Reagent, [Reagent])
+
+-- Not working, the stock' values are all independent, they should share insertions
+oreFor :: Map String Reaction -> Map String Int -> Reagent -> Int
+oreFor _ s ("ORE", n) = trace (show s) n
+oreFor grimoire stock (name, needed) = nbReactions * (sum $ map (oreFor grimoire stock') ingredients)
+  where ((_, provided), ingredients) = grimoire ! name :: Reaction
+        available = Map.findWithDefault 0 name stock
+        needed' = if available < needed then needed - available else 0
+        nbReactions = ceiling $ (fromIntegral (needed') / fromIntegral (provided))
+        stock' = Map.insertWith (+) name (nbReactions*provided - needed') stock
+
+loadExample = foldl storeByProductName Map.empty . map parseLine
 
 storeByProductName m r = Map.insert name r m
   where name = fst $ fst r
