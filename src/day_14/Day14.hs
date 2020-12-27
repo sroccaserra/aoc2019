dfddef025c23f229aea98c35395590968ada7098
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-27 22:43:22 +0100

    [day 14] Exploring adding more state

--- src/day_14/Day14.hs
+++ src/day_14/Day14.hs
@@ -6,17 +6,28 @@ import Data.Map (Map, (!), empty)
 import Text.ParserCombinators.ReadP
 import Debug.Trace
 
-partOne reactions = oreFor grimoire empty ("FUEL", 1)
+partOne reactions = oreFor grimoire (0,empty) ("FUEL", 1)
   where grimoire = foldl storeByProductName empty reactions
 
 type Reagent = (String, Int)
 type Reaction = (Reagent, [Reagent])
 
+type Grimoire = Map String Reaction
+type Stock = Map String Int
+
+type State = (Int, Stock)
+
+-- stepReaction :: Grimoire -> State -> State
+-- stepReaction g (((_, _), [("ORE", k)]), n, s) = ((("ORE", k), []), n+k, s)
+-- stepReaction g (((i, p), i':rest), n, s) = stepReaction g (((i, p), rest), n', s')
+--   where ((_, provided), )
+
 -- Not working, the stock' values are all independent, they should share insertions
-oreFor :: Map String Reaction -> Map String Int -> Reagent -> Int
-oreFor _ s ("ORE", n) = trace (show s) n
-oreFor grimoire stock (name, needed) = nbReactions * (sum $ map (oreFor grimoire stock') ingredients)
-  where ((_, provided), ingredients) = grimoire ! name :: Reaction
+oreFor :: Grimoire -> State -> Reagent -> State
+oreFor _ (s,stock) ("ORE", n) = (n+s, stock)
+oreFor grimoire (s,stock) (name, needed) = foldl (\(n, s) (n', s') -> (n+n', Map.unionWith (+) s s')) (0,stock') results
+  where results = map (oreFor grimoire (s,stock')) ingredients
+        ((_, provided), ingredients) = grimoire ! name :: Reaction
         available = Map.findWithDefault 0 name stock
         needed' = if available < needed then needed - available else 0
         nbReactions = ceiling $ (fromIntegral (needed') / fromIntegral (provided))
@@ -38,8 +49,8 @@ main = interact $ show . partOne . map parseLine . lines
 parseLine = fst . last . readP_to_S parser
 
 parser = do
-  reagents <- sepBy1 reagent (string ", ")
-  _ <- string " => "
+  reagents <- reagent `sepBy` (string ", ")
+  string " => "
   product <- reagent
   return (product, reagents)
 
