1559d1f64f60dffc061da56c30042138c97f5d08
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-28 10:23:07 +0100

    [day 14] Solve part one

--- src/day_14/Day14.hs
+++ src/day_14/Day14.hs
@@ -2,41 +2,35 @@ module Day14 where
 
 import Data.Char
 import qualified Data.Map as Map
-import Data.Map (Map, (!), empty)
+import Data.Map (Map, (!))
 import Text.ParserCombinators.ReadP
-import Debug.Trace
 
-partOne reactions = oreFor grimoire (0,empty) ("FUEL", 1)
-  where grimoire = foldl storeByProductName empty reactions
+partOne reactions =
+  Map.lookup "ORE" $ until fullfiled (stepReaction grimoire) (Map.singleton "FUEL" 1)
+  where grimoire = foldl storeByProductName Map.empty reactions
 
 type Reagent = (String, Int)
 type Reaction = (Reagent, [Reagent])
 
 type Grimoire = Map String Reaction
-type Stock = Map String Int
+type Required = Map String Int
 
-type State = (Int, Stock)
+storeByProductName m r@((name,_),_) = Map.insert name r m
 
--- stepReaction :: Grimoire -> State -> State
--- stepReaction g (((_, _), [("ORE", k)]), n, s) = ((("ORE", k), []), n+k, s)
--- stepReaction g (((i, p), i':rest), n, s) = stepReaction g (((i, p), rest), n', s')
---   where ((_, provided), )
+fullfiled = Map.null . toProduce
 
--- Not working, the stock' values are all independent, they should share insertions
-oreFor :: Grimoire -> State -> Reagent -> State
-oreFor _ (s,stock) ("ORE", n) = (n+s, stock)
-oreFor grimoire (s,stock) (name, needed) = foldl (\(n, s) (n', s') -> (n+n', Map.unionWith (+) s s')) (0,stock') results
-  where results = map (oreFor grimoire (s,stock')) ingredients
-        ((_, provided), ingredients) = grimoire ! name :: Reaction
-        available = Map.findWithDefault 0 name stock
-        needed' = if available < needed then needed - available else 0
-        nbReactions = ceiling $ (fromIntegral (needed') / fromIntegral (provided))
-        stock' = Map.insertWith (+) name (nbReactions*provided - needed') stock
+toProduce = Map.delete "ORE" . Map.filter (> 0)
 
-loadExample = foldl storeByProductName Map.empty . map parseLine
+stepReaction :: Grimoire -> Required -> Required
+stepReaction g reqs = foldl (require neededN) reqs' reagents
+  where (needed, neededQ) = Map.findMin $ toProduce reqs
+        ((_,providedQ), reagents) = g ! needed
+        neededN = max 1 (div neededQ providedQ)
+        reqs' = produce neededN reqs (needed, providedQ)
 
-storeByProductName m r = Map.insert name r m
-  where name = fst $ fst r
+produce n reqs (name,q) = Map.insertWith (flip (-)) name (n*q) reqs
+
+require n reqs (name,q) = Map.insertWith (+) name (n*q) reqs
 
 ---
 -- Main
