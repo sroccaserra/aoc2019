67ca8a652ca631b3f6829ec56e43c4d713aad1a2
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-29 17:27:29 +0100

    [day 15] Adapt code for maze exploration

--- src/day_15/main.clj
+++ src/day_15/main.clj
@@ -1,105 +1,59 @@
-(ns day-13.main
+(ns day-15.main
   (:gen-class)
   (:require [clojure.string :as str]
             [aoc-common-cli :refer [read-intcode-program-from-stdin]]
             [intcode.vm-state :refer [create-intcode-vm add-input drop-outputs]]
             [intcode.run :refer [run run-until-needs-input]]))
 
-(def tiles {0 \.
-            1 \█
-            2 \▒
-            3 \=
-            4 \o})
+(defn create-maze-state []
+  {:visited {[0 0] \.}
+   :robot [0 0]})
 
-(def w 42)
-(def h 24)
+(defn update-maze-state [maze-state output]
+  maze-state)
 
-(def empty-vram (into [] (repeat (* w h) \?)))
+(defn maze-state-as-string [{:keys [:visited :robot]}]
+  (let [coords (keys visited)
+        xs (map first coords)
+        ys (map second coords)
+        [x-min y-min x-max y-max] [(apply min xs) (apply min ys) (apply max xs) (apply max ys)]
+        tiles (for [x (range x-min (inc x-max))
+                    y (range y-min (inc y-max))]
+                (if (= [x y] robot)
+                  \@
+                  (visited [x y] \▒)))]
+    (->> tiles
+         (partition (inc (- x-max x-min)))
+         (map #(apply str %))
+         (str/join "\n"))))
 
 (defn reset-terminal []
   (println  "\033c"))
 
-(defn vram-to-str [vram]
-  (->> vram
-       (partition w)
-       (map #(apply str %))
-       (str/join "\n")))
+(defn draw-maze [maze-state]
+  (comment reset-terminal)
+  (println (maze-state-as-string maze-state)))
 
-(defn draw-tile-to-vram [vram [x y tile-id]]
-  (assoc vram (+ (* y w) x) (tiles tile-id)))
+(defn has-found-oxygen [maze-state]
+  true)
 
-(defn has-found-oxygen [vram]
-  (some #(= (tiles 2) %) vram))
+(defn update-vm-state [vm-state maze-state]
+  vm-state)
 
-(defn is-score-tile [tile]
-  (= [-1 0] (take 2 tile)))
-
-(defn find-paddle-tile [tiles]
-  (some #(when (= 3 (nth % 2)) %) tiles))
-
-(defn find-ball-tile [tiles]
-  (some #(when (= 4 (nth % 2)) %) tiles))
-
-(defn find-score-tile [tiles]
-  (some #(when (is-score-tile %) %) tiles))
-
-(defn read-output [outputs]
-  (let [tiles (partition 3 outputs)]
-    {:paddle-x (first (find-paddle-tile tiles))
-     :ball-x (first (find-ball-tile tiles))
-     :score (nth (find-score-tile tiles) 2)
-     :tiles (remove is-score-tile tiles)}))
-
-(defn update-vm-state [vm-state {:keys [paddle-x ball-x]}]
-  (let [joystick-state (cond (nil? paddle-x) 0
-                             (< paddle-x ball-x) 1
-                             (> paddle-x ball-x) -1
-                             :else 0)]
-    (prn joystick-state)
-    (-> vm-state
-        drop-outputs
-        (add-input joystick-state))))
-
-(defn draw-game-state [{:keys [:score :vram]}]
-  (reset-terminal)
-  (println (vram-to-str vram))
-  (println "score:" score))
-
-(defn create-game-state []
-  {:paddle-x nil
-   :ball-x nil
-   :score nil
-   :vram empty-vram})
-
-(defn update-vram [{vram :vram :as game-state} tiles]
-  (assoc game-state
-         :vram
-         (reduce draw-tile-to-vram vram tiles)))
-
-(defn not-nil-keys [m ks]
-  (->> (select-keys m ks)
-       (filter #(not (nil? (second %))))))
-
-(defn update-game-state [game-state outputs]
-  (let [new-values (read-output outputs)]
-    (-> game-state
-        (merge (not-nil-keys new-values [:paddle-x :ball-x :score]))
-        (update-vram (:tiles new-values)))))
-
-(defn play-game
+(defn explore-maze
   ([vm-state]
-   (play-game vm-state (create-game-state)))
-  ([vm-state game-state]
+   (explore-maze vm-state (create-maze-state)))
+  ([vm-state maze-state]
    (let [next-vm-state (run-until-needs-input vm-state)
-         game-state' (update-game-state game-state
+         maze-state' (update-maze-state maze-state
                                         (:outputs next-vm-state))]
-     (draw-game-state game-state')
+     (draw-maze maze-state')
      (comment Thread/sleep 25)
-     (if (has-found-oxygen (:vram game-state'))
+     (if (has-found-oxygen maze-state')
        next-vm-state
-       (recur (update-vm-state next-vm-state game-state') game-state')))))
+       (recur (update-vm-state next-vm-state maze-state') maze-state')))))
 
 (defn -main [& args]
   (let [program (read-intcode-program-from-stdin)
         vm (create-intcode-vm program :memory-size 2659)]
-    (play-game vm )))
+    (explore-maze vm )))
