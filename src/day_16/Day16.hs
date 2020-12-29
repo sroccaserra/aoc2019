5230dc105d810f1feb0474e2b60dcef94bcc513c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-30 00:23:44 +0100

    [day 16] Solve part one

--- src/day_16/Day16.hs
+++ src/day_16/Day16.hs
@@ -1,10 +1,15 @@
 module Day16 where
 
-partOne _ = map (take 10) $ map pattern [1, 2, 3]
+partOne = take 8 . last . take 101 . iterate applyPhase
+
+applyPhase xs = zipWith ($) transforms (repeat xs)
+  where transforms = map transform [1..length xs]
 
 basePattern = [0, 1, 0, -1]
 
 pattern n = drop 1 $ cycle $ concatMap (replicate n) basePattern
+
+transform n = (`mod` 10) . abs . sum . zipWith (*) (pattern n)
 
 main = interact $ show . partOne . parse
 
