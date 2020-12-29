a9aa43be78825ea8b6261e1faa1b037744a06cdd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-30 00:05:54 +0100

    [day 16] Generate pattern

--- src/day_16/Day16.hs
+++ src/day_16/Day16.hs
@@ -1,10 +1,12 @@
 module Day16 where
 
-import Data.List
+partOne _ = map (take 10) $ map pattern [1, 2, 3]
+
+basePattern = [0, 1, 0, -1]
+
+pattern n = drop 1 $ cycle $ concatMap (replicate n) basePattern
 
 main = interact $ show . partOne . parse
 
 parse :: String -> [Int]
 parse = map read . map (:[]) . head . lines
-
-partOne = id
