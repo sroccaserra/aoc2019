40eb8240fe8e4719cd326e47f159047f2999b285
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-31 18:07:42 +0100

    [day 16] Add toNum function

--- src/day_16/Day16.hs
+++ src/day_16/Day16.hs
@@ -1,15 +1,23 @@
 module Day16 where
 
-partOne = take 8 . last . take 101 . iterate applyPhase
+---
+-- Part one
+
+partOne = toNum . take 8 . last . take 101 . iterate applyPhase
 
 applyPhase xs = zipWith ($) transforms (repeat xs)
   where transforms = map transform [1..length xs]
 
 basePattern = [0, 1, 0, -1]
 
-pattern n = drop 1 $ cycle $ concatMap (replicate n) basePattern
+pattern = drop 1 . cycle . (`concatMap` basePattern) . replicate
 
 transform n = (`mod` 10) . abs . sum . zipWith (*) (pattern n)
+
+toNum = foldl1 ((+) . (*10))
+
+---
+-- Main
 
 main = interact $ show . partOne . parse
 
