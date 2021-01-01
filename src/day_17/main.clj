b9d5b40bc7cc2ad6ffb9115e9eb5377c3375a649
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-01 12:58:51 +0100

    [day 17] Split image in lines

--- src/day_17/main.clj
+++ src/day_17/main.clj
@@ -8,13 +8,20 @@
 (defn reset-terminal []
   (println  "\033c"))
 
-(defn display-scaffolding [outputs]
+(defn display-scaffolding [lines]
+  (->> lines
+       (str/join "\n")
+       println))
+
+(defn as-lines [outputs]
   (->> outputs
-      (map char)
-      (apply str)
-      println))
+       (map char)
+       (apply str)
+       str/split-lines))
 
 (defn -main [& args]
   (let [program (read-intcode-program-from-file "resources/day_17/input.txt")
         vm (create-intcode-vm program :inputs [] :memory-size 4000)]
-    (display-scaffolding (:outputs (run-until-needs-input vm)))))
+    (-> (:outputs (run-until-needs-input vm))
+        as-lines
+        display-scaffolding)))
