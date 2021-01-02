e07b93d4aea79a3634807238d4b80178f25adcb4
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-02 21:14:14 +0100

    [day 18] Compute distance to start in the whole maze

--- src/day_18/main.py
+++ src/day_18/main.py
@@ -1,12 +1,66 @@
 import fileinput
+import collections
 import sys
 
 
-def parse(lines):
-    for line in lines:
-        print(line)
+def solve(lines):
+    start = find_start(lines)
+    return compute_distances(lines, start)
+
+
+def find_start(lines):
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            if char_at(lines, (x, y)) == '@':
+                return (x, y)
+
+
+def compute_distances(lines, start):
+    distances = {start: 0}
+    to_explore = collections.deque([start])
+    while to_explore:
+        point = to_explore.popleft()
+        for neighbor in neighbors(point):
+            if not is_in_bounds(lines, neighbor):
+                continue
+            if '#' == char_at(lines, neighbor):
+                continue
+            if neighbor in distances:
+                continue
+            distances[neighbor] = distances[point] + 1
+            to_explore.append(neighbor)
+    return distances
+
+
+def char_at(lines, point):
+    x, y = point
+    return lines[y][x]
+
+
+def is_in_bounds(lines, point):
+    x, y = point
+    return (0 <= x < len(lines[0])) and (0 <= y < len(lines))
+
+
+def neighbors(point):
+    x, y = point
+    return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+
+
+def display_maze(lines, distances):
+    for y in range(0, len(lines)):
+        for x in range(0, len(lines[0])):
+            p = (x, y)
+            if p in distances:
+                d = distances[p]
+                print(d % 10, end='')
+            else:
+                c = char_at(lines, p)
+                print(c, end='')
+        print('')
 
 
 if __name__ == '__main__' and not sys.flags.interactive:
-    lines = [l.strip() for l in fileinput.input()]
-    parse(lines)
+    lines = [line.strip() for line in fileinput.input()]
+    distances = solve(lines)
+    display_maze(lines, distances)
