b169f5ccc6b238993ff1dc55fd2ca3cbcc5e0724
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-01-10 19:23:43 +0100

    [day 18] Haskell solution: find start in grid

--- src/day_18/Day18.hs
+++ src/day_18/Day18.hs
@@ -1,5 +1,25 @@
-solve = id
+import Data.Array.Unboxed
+import Data.Foldable
+
+solve :: [String] -> Point
+solve xs = findStart grid
+  where w = length $ head xs
+        h = length xs
+        grid = Grid w h $ listArray (0, w*h - 1) $ concat xs
+
+data Grid = Grid Int Int (UArray Int Char)
+          deriving (Show)
+
+type Point = (Int, Int)
+
+charAt :: Grid -> Point -> Char
+charAt (Grid w _ v) (x, y) = v ! (y*w + x)
+
+findStart :: Grid -> Point
+findStart (Grid w _ v) = (div i w, mod i w)
+  where Just (i, _) = find ((== '@') . snd) $ assocs v
 
 main = do
   input <- getContents
-  mapM_ putStrLn $ solve $ lines input
+  let grid = solve $ lines input
+  putStrLn $ show . solve $ lines input
