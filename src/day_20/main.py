38ae3a6a7b6c511137cb8844a599f25271aa9404
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-04 22:18:12 +0200

    [day 20] Solve part one

--- src/day_20/main.py
+++ src/day_20/main.py
@@ -1,4 +1,5 @@
 import fileinput
+from queue import Queue
 
 UNSEEN = -1
 A = ord('A')
@@ -6,9 +7,49 @@ Z = ord('Z')
 
 
 def solve(lines):
-    labyrinth = create_labyrinth(lines)
-    portals = find_portals(labyrinth)
-    return [p for p in portals if len(portals[p]) != 2]
+    m = create_labyrinth(lines)
+    portals = find_portals(m)
+    AA = portals['AA'][0]
+    ZZ = portals['ZZ'][0]
+    wormholes = dict()
+    print(portals)
+    for [a, b] in [vv for vv in portals.values() if len(vv) == 2]:
+        wormholes[a] = b
+        wormholes[b] = a
+    mark(m, wormholes, ZZ, AA)
+    show(m, lines)
+    return m[ZZ]
+
+
+def show(m, lines):
+    for y in range(H):
+        print()
+        for x in range(W):
+            n = m.get((x, y))
+            c = lines[y][x]
+            if type(n) is int:
+                print(str(n).rjust(3), end='')
+            elif c == '#' or is_letter(c):
+                print(c.center(3, ':'), end='')
+            else:
+                print('   ', end='')
+    print()
+
+
+def mark(m, wormholes, ZZ, AA):
+    todo = Queue()
+    todo.put(AA)
+    m[AA] = 0
+    while m[ZZ] is UNSEEN:
+        p = todo.get()
+        n = m.get(p)
+
+        for neighbor in neighbors(m, wormholes, p[0], p[1]):
+            if m[neighbor] is not UNSEEN:
+                continue
+
+            m[neighbor] = n+1
+            todo.put(neighbor)
 
 
 def create_labyrinth(lines):
@@ -56,11 +97,15 @@ def find_portals(m):
     return portals
 
 
-def neighbors(m, x, y):
-    return [m.get((x+i, y+j)) for i, j in [
-        (-1, -1), (0, -1), (1, -1),
-        (-1,  0),          (1,  0),
-        (-1,  1), (0,  1), (1,  1)]]
+def neighbors(m, wormholes, x, y):
+    result = []
+    for p in [(x, y-1), (x-1,  y), (x+1,  y), (x,  y+1)]:
+        if type(m.get(p)) is int:
+            result.append((p))
+    wh = wormholes.get((x, y))
+    if wh is not None:
+        result.append(wh)
+    return result
 
 
 lines = [line for line in fileinput.input()]
