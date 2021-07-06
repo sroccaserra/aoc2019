1927ab40c4d44917a7f1702b6a6eedbef7a41571
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-07 00:06:04 +0200

    [day 21] Solve part two

--- src/day_21/main.clj
+++ src/day_21/main.clj
@@ -5,11 +5,58 @@
             [intcode.vm-state :refer [create-intcode-vm add-inputs write-int-at]]
             [intcode.run :refer [run run-until-needs-input]]))
 
-(def program "NOT A J
+; @
+; #ABCDEFGHI
+
+; @
+; #.########
+
+; @
+; #..#######
+
+; not a j
+; @
+; #...######
+; @
+; #.#.#...##
+;  ABCDEFGHI
+
+; not c t and d t and i t
+; @
+; ###.#..###
+;  ABCDEFGHI
+
+
+; not c t and d t and h t
+; @
+; ###.#...#.#
+;  ABCDEFGHI
+
+; prevent this:
+; @
+; ###.#.##.#
+;  ABCDEFGHI
+
+; not b t and d t
+; @
+; ##.##.####
+;  ABCDEFGHI
+
+(def program "NOT C J
+             AND D J
+             AND I J
+             NOT F T
+             AND T J
+             NOT A T
+             OR T J
              NOT C T
              AND D T
+             AND H T
              OR T J
-             WALK
+             NOT B T
+             AND D T
+             OR T J
+             RUN
              ")
 
 (defn -main [& args]
