458f4dfc72c4ce0a3a10a87d0c0feec39a178cd6
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-14 02:38:52 +0200

    [day 22] WIP: invert shuffles

--- src/day_22/main.py
+++ src/day_22/main.py
@@ -9,7 +9,10 @@ NB_CARDS = 10007
 def main():
     lines = [line.strip() for line in fileinput.input()]
     shuffles = parse(lines)
-    print(solve_1(2019, shuffles))
+    i = solve_1(2019, shuffles)
+    print(i)
+    shuffle = invert(reduce(combine, shuffles, (1, 0)))
+    print((shuffle[0]*i + shuffle[1]) % NB_CARDS)
 
 
 def solve_1(i, shuffles):
@@ -36,7 +39,7 @@ def parse_line(line):
 
 # c(ax + b) + d -> cax + cb + d
 def combine(f, g):
-    return (f[0]*g[0], f[1]*g[0] + g[1])
+    return ((f[0]*g[0]) % NB_CARDS, (f[1]*g[0] + g[1]) % NB_CARDS)
 
 
 def cut(n):
@@ -49,6 +52,31 @@ def new_stack():
 
 def increment(n):
     return (n, 0)
+
+
+# trouver c et d tels que
+# c(ax + b) + d -> cax + cb + d -> x
+def invert(f):
+    c = mul_inv(f[0], NB_CARDS)
+    d = (-c*f[1]) % NB_CARDS
+    return (c, d)
+
+
+def mul_inv(a, b):
+    """
+    - https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+    """
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
 
 
 if __name__ == '__main__' and not sys.flags.interactive:
