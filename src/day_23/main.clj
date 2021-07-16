84552631b7f32df90d8eccd6b62758e2e54839dd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-16 23:52:22 +0200

    [day 23] Solve part one

--- src/day_23/main.clj
+++ src/day_23/main.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as str]
             [aoc-common-cli :refer [read-intcode-program-from-file]]
             [intcode.vm-state :refer [create-intcode-vm add-inputs write-int-at]]
-            [intcode.run :refer [run run-until-needs-input]]))
+            [intcode.run :refer [run-until-needs-input]]))
 
 (def nb-vms 50)
 
@@ -17,12 +17,18 @@
 (defn list-packets [vms]
   (partition 3 (mapcat :outputs vms)))
 
-(defn step [vms]
+(defn find-first-255-packet [packets]
+  (first (filter #(= 255 (first %)) packets)))
+
+(defn run [vms]
   (let [vms' (vec (map run-until-needs-input vms))
-        packets (list-packets vms')]
-    (dispatch-packets packets vms')))
+        packets (list-packets vms')
+        found (find-first-255-packet packets)]
+    (if found
+      found
+      (recur (dispatch-packets packets vms')))))
 
 (defn -main [& args]
   (let [intcode-program (read-intcode-program-from-file "resources/day_23/input.txt")
-        vms (vec (map #(create-intcode-vm intcode-program :inputs [% -1]) (range nb-vms)))]
-          (run! prn (list-packets (step vms)))))
+        vms (vec (map #(create-intcode-vm intcode-program :inputs [% -1] :memory-size 4000) (range nb-vms)))]
+          (prn (last (run vms)))))
