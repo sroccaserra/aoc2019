8f514a327c4204425600519e0f7e54758bac5a71
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-16 23:39:06 +0200

    [day 23] Dispatch packets

--- src/day_23/main.clj
+++ src/day_23/main.clj
@@ -7,13 +7,22 @@
 
 (def nb-vms 50)
 
-(defn step [vms]
-  (map run-until-needs-input vms))
+(defn dispatch-packet [vms [address x y]]
+  (let [vm (nth vms address)]
+    (assoc vms address (add-inputs vm [x y]))))
 
-(defn packets [vms]
+(defn dispatch-packets [packets vms]
+  (reduce dispatch-packet vms packets))
+
+(defn list-packets [vms]
   (partition 3 (mapcat :outputs vms)))
+
+(defn step [vms]
+  (let [vms' (vec (map run-until-needs-input vms))
+        packets (list-packets vms')]
+    (dispatch-packets packets vms')))
 
 (defn -main [& args]
   (let [intcode-program (read-intcode-program-from-file "resources/day_23/input.txt")
-        vms (map #(create-intcode-vm intcode-program :inputs [% -1]) (range nb-vms))]
-          (run! prn (packets (step vms)))))
+        vms (vec (map #(create-intcode-vm intcode-program :inputs [% -1]) (range nb-vms)))]
+          (run! prn (list-packets (step vms)))))
