9e44ef684f0560f8779132f4cfeaa49d00e11cbf
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-20 23:51:41 +0200

    [day 25] Correct order of operations in repl

--- src/day_25/main.clj
+++ src/day_25/main.clj
@@ -8,19 +8,25 @@
 (defn encode-command [command]
   (concat (map int command) [10]))
 
-(defn decode-output [output]
-  (apply str (map char output)))
-
 (defn execute-command [vm command]
   (run-until-needs-input (add-inputs vm (encode-command command))))
 
+(defn decode-output [output]
+  (apply str (map char output)))
+
+(defn print-output [vm]
+  (println (decode-output (:outputs vm))))
+
 (defn repl [vm]
-  (println (decode-output (:outputs vm)))
   (let [command (read-line)
         vm' (execute-command (drop-outputs vm) command)]
+    (print-output vm')
     (recur vm')))
 
 (defn -main [& args]
   (let [intcode-program (read-intcode-program-from-file "resources/day_25/input.txt")
-        vm (create-intcode-vm intcode-program :inputs [] :memory-size 8000)]
-    (repl (run-until-needs-input vm))))
+        vm (-> intcode-program
+               (create-intcode-vm :inputs [] :memory-size 8000)
+               run-until-needs-input)]
+    (print-output vm)
+    (repl vm)))
