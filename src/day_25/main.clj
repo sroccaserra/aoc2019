bf99349233771eafce189d0271a782b1fba84669
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-07-20 23:23:01 +0200

    [day 25] Name and shuffle a few things in the repl

--- src/day_25/main.clj
+++ src/day_25/main.clj
@@ -5,15 +5,22 @@
             [intcode.vm-state :refer [create-intcode-vm add-inputs drop-outputs]]
             [intcode.run :refer [run-until-needs-input]]))
 
+(defn encode-command [command]
+  (concat (map int command) [10]))
+
+(defn decode-output [output]
+  (apply str (map char output)))
+
+(defn execute-command [vm command]
+  (run-until-needs-input (add-inputs vm (encode-command command))))
+
 (defn repl [vm]
+  (println (decode-output (:outputs vm)))
   (let [command (read-line)
-        vm' (run-until-needs-input (add-inputs vm (concat (map int command) [10])))
-        result (:outputs vm')
-        vm'' (drop-outputs vm')]
-    (println (apply str (map char result)))
-    (recur vm'')))
+        vm' (execute-command (drop-outputs vm) command)]
+    (recur vm')))
 
 (defn -main [& args]
   (let [intcode-program (read-intcode-program-from-file "resources/day_25/input.txt")
         vm (create-intcode-vm intcode-program :inputs [] :memory-size 8000)]
-    (repl vm)))
+    (repl (run-until-needs-input vm))))
